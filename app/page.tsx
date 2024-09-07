61ada15114d406a736895d6f14771d19db13a238
Brian Temu <bryangasper2124@gmail.com>
2024-09-07 19:14:51 -0400

    rag

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen bg-blue-900 justify-center items-center">
+      <h1 className="text-xl font-semibold">Rag Project</h1>
+    </div>
+
+  );
+}
