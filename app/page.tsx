1dcfb32031c4dd43ea4ff9b3f8eb642d9ae93202
Brian Temu <bryangasper2124@gmail.com>
2024-09-12 20:42:16 -0400

    chat text

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
 export default function Home() {
   return (
     <div className="flex h-screen flex-row">
-      <div className="w-[20%] bg-blue-200"></div>
-      <div className="w-[80%]">
-        <div className="w-full h-[92%] bg-red-400"></div>
+      <div className="w-[20%] bg-blue-200">
+        <div className="flex justify-center items-center h-32">
+          <div className="h-12 bg-red-800 w-[80%] rounded-full flex justify-center items-center">
+            <h1 className="text-xl font-medium"> Upload PDF</h1>
+          </div>
+        </div>
+      </div>
 
+      <div className="w-[80%]">
+        <div className="h-[10%] w-full bg-green-50 flex items-center px-12 border-b-2 border-black">
+          <h1 className="text-black font-sans text-2xl font-semibold">
+            ChatRAG
+          </h1>
+        </div>
+
+        {/* Chat Interface */}
+        <div className="w-full h-[82%] bg-red-400 flex justify-center ">
+          <div className="w-[70%] h-full bg-blue-600">
+            {/* Query Text */}
+            <div className="flex items-center justify-end bg-white">
+              <div className="max-w-[70%] bg-yellow-300 p-2 rounded-md">
+                <h1 className="text-black">Query text here</h1>
+              </div>
+            </div>
+
+            {/* Response  Text */}
+            <div className="flex items-center bg-white">
+              <div className="max-w-[70%] bg-yellow-300 p-2 rounded-md">
+                <h1 className="text-black">Query text here</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Input  */}
         <div className="w-full bg-yellow-50 h-[8%] flex justify-center items-center">
           <div className="h-14 w-[60%] bg-white rounded-full px-4 flex items-center border-2 border-black justify-between">
             <input
@@ -27,7 +58,6 @@ export default function Home() {
                   d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
                 />
               </svg>
-              
             </div>
           </div>
         </div>
