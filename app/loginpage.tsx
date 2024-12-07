9190c4180e4b4a0c3fd4744cc549c44e90f7f7f8
Brian Temu <bryangasper2124@gmail.com>
2024-12-06 21:23:07 -0500

    View Password

--- app/loginpage.tsx
+++ app/loginpage.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { useState } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+
 export default function LoginPage() {
+  const { data: session } = useSession();
   const [showPassword, setShowPassword] = useState(false);
   const togglePasswordVisibility = () => {
     setShowPassword((prevState) => !prevState);
@@ -43,87 +47,102 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-[50%] h-[50%] p-6">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-3xl font-semibold">Sign In</h1>
-          <h1 className="text-lg">Enter your email and password to sign in</h1>
-        </div>
+    // <div className="flex justify-center items-center h-screen">
+    //   <div className="w-[50%] h-[50%] p-6">
+    //     <div className="flex flex-col justify-center items-center">
+    //       <h1 className="text-3xl font-semibold">Sign In</h1>
+    //       <h1 className="text-lg">Enter your email and password to sign in</h1>
+    //     </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-2 " action="#" method="POST">
-            <div>
-              <label
-                for="email"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  placeholder="Email"
-                  type="email"
-                  name="email"
-                  id="email"
-                  autocomplete="email"
-                  required
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                for="email"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Password
-              </label>
-              <div className="relative flex items-center">
-                <input
-                  placeholder="Password"
-                  type={showPassword ? "text" : "password"}
-                  name="password"
-                  id="password"
-                  required
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 pr-10"
-                />
-                <button
-                  type="button"
-                  onClick={togglePasswordVisibility}
-                  className="absolute right-3 text-gray-500 hover:text-gray-700 focus:outline-none"
-                >
-                  {showPassword ? <ViewEye /> : <NotViewEye />}
-                </button>
-              </div>
-            </div>
-            <div className="flex justify-end">
-              <h1 className="font-semibold">Forgot Password ?</h1>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-          <button className="mt-6 w-full flex h-10 items-center justify-center gap-2 border rounded-md">
-            <img
-              src={`https://www.material-tailwind.com/logos/logo-google.png`}
-              alt="google"
-              className="h-6 w-6"
-            />
-            <h1 className="font-medium">Sign in with Google</h1>
-          </button>
-          <div className="flex space-x-1 justify-center py-2">
-            <h1>Not registered?</h1>
-            <a href="#" className="font-bold text-gray-900">
-              Create account
-            </a>
-          </div>
-        </div>
-      </div>
+    //     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    //       <form className="space-y-2 " action="#" method="POST">
+    //         <div>
+    //           <label
+    //             for="email"
+    //             className="block text-sm/6 font-medium text-gray-900"
+    //           >
+    //             Email address
+    //           </label>
+    //           <div className="mt-2">
+    //             <input
+    //               placeholder="Email"
+    //               type="email"
+    //               name="email"
+    //               id="email"
+    //               autocomplete="email"
+    //               required
+    //               className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+    //             />
+    //           </div>
+    //         </div>
+    //         <div>
+    //           <label
+    //             for="email"
+    //             className="block text-sm/6 font-medium text-gray-900"
+    //           >
+    //             Password
+    //           </label>
+    //           <div className="relative flex items-center">
+    //             <input
+    //               placeholder="Password"
+    //               type={showPassword ? "text" : "password"}
+    //               name="password"
+    //               id="password"
+    //               required
+    //               className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 pr-10"
+    //             />
+    //             <button
+    //               type="button"
+    //               onClick={togglePasswordVisibility}
+    //               className="absolute right-3 text-gray-500 hover:text-gray-700 focus:outline-none"
+    //             >
+    //               {showPassword ? <ViewEye /> : <NotViewEye />}
+    //             </button>
+    //           </div>
+    //         </div>
+    //         <div className="flex justify-end">
+    //           <h1 className="font-semibold">Forgot Password ?</h1>
+    //         </div>
+    //         <div>
+    //           <button
+    //             type="submit"
+    //             className="flex w-full justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    //           >
+    //             Sign in
+    //           </button>
+    //         </div>
+    //       </form>
+    //       <button className="mt-6 w-full flex h-10 items-center justify-center gap-2 border rounded-md">
+    //         <img
+    //           src={`https://www.material-tailwind.com/logos/logo-google.png`}
+    //           alt="google"
+    //           className="h-6 w-6"
+    //         />
+    //         <h1 className="font-medium">Sign in with Google</h1>
+    //       </button>
+    //       <div className="flex space-x-1 justify-center py-2">
+    //         <h1>Not registered?</h1>
+    //         <a href="#" className="font-bold text-gray-900">
+    //           Create account
+    //         </a>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      {!session ? (
+        <>
+          <h1>Welcome to NextAuth.js with Google Sign-In</h1>
+          <button onClick={() => signIn("google")}>Sign in with Google</button>
+        </>
+      ) : (
+        <>
+          <h1>Welcome, {session.user.name}!</h1>
+          <p>Email: {session.user.email}</p>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
     </div>
   );
 }
