bef8dc4002c6da9a627e213f977d06d517b281e4
Brian Temu <bryangasper2124@gmail.com>
2024-12-11 23:25:00 -0500

    chromadb

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -17,7 +17,7 @@ const handleFileChange = (event) => {
     'application/pdf',
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
   ];
-  const maxSize = 10 * 1024 * 1024; // 5MB
+  const maxSize = 10 * 1024 * 1024; // 10MB
 
   if (selectedFile) {
     if (!allowedTypes.includes(selectedFile.type)) {
@@ -60,102 +60,105 @@ const handleUpload = async () => {
   }
 };
 
-return (
-  <div>
-    <h1>File Upload</h1>
-    <input type="file" onChange={handleFileChange} />
-    <button onClick={handleUpload}>Upload</button>
-    {message && <p>{message}</p>}
-  </div>
-);
+// return (
+//   <div>
+//     <h1>File Upload</h1>
+//     <input type="file" onChange={handleFileChange} />
+//     <button onClick={handleUpload}>Upload</button>
+//     {message && <p>{message}</p>}
+//   </div>
+// );
 
-  // return (
-  //   <div className="flex h-screen flex-row bg-slate-800">
-  //     <div className="w-[20%] bg-slate-300">
-        
+  return (
+    <div className="flex h-screen flex-row bg-slate-800">
+      <div className="w-[20%] bg-slate-300">
+        <div className="flex flex-col justify-center items-center h-32">
+          {/* <input
+          type="file"
+            // htmlFor="fileUpload"
+            className="h-12 bg-gray-800 w-[80%] rounded-full flex justify-center items-center"
+          >
+            <a>
+              <h1 className="text-xl font-medium text-white"> Upload PDF</h1>
+            </a>
+          </input> */}
 
-  //       <div className="flex justify-center items-center h-32">
-  //         <label
-  //           htmlFor="fileUpload"
-  //           className="h-12 bg-gray-800 w-[80%] rounded-full flex justify-center items-center"
-  //         >
-  //           <a>
-  //             <h1 className="text-xl font-medium text-white"> Upload PDF</h1>
-  //           </a>
-  //         </label>
-  //       </div>
-  //       <input id="fileUpload" type="file" className="hidden" />
-  //     </div>
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={handleUpload}>Upload</button>
+          {message && <p>{message}</p>}
+        </div>
+        <input id="fileUpload" type="file" className="hidden" />
+      </div>
 
-  //     <div className="w-[80%]">
-  //       <div className="h-[10%] w-full  flex items-center px-12 border-b-2 border-white">
-  //         <h1 className="text-white font-sans text-2xl font-semibold">
-  //           ChatRAG
-  //         </h1>
-  //       </div>
+      <div className="w-[80%]">
+        <div className="h-[10%] w-full  flex items-center px-12 border-b-2 border-white">
+          <h1 className="text-white font-sans text-2xl font-semibold">
+            ChatRAG
+          </h1>
+        </div>
 
-  //       {/* Chat Interface */}
-  //       <div className="w-full h-[82%]  flex justify-center pt-4">
-  //         <div className="w-[70%] h-full ">
-  //           {/* Query Text */}
-  //           <div className="flex items-center justify-end ">
-  //             <div className="max-w-[70%] bg-gray-100 p-2 rounded-md">
-  //               <h1 className="text-black">
-  //                 Lorem Ipsum is simply dummy text of the printing and
-  //                 typesetting industry. Lorem Ipsum has been the industrys
-  //                 standard dummy text ever since the 1500s, when an unknown
-  //                 printer took a galley of type and scrambled it to make a type
-  //                 specimen book. It has survived not only five centuries, but
-  //                 also the leap into electronic typesetting, remaining
-  //                 essentially unchanged. It was popularised in the 1960s with
-  //                 the release of Letraset sheets containing Lorem Ipsum
-  //                 passages, and more recently with desktop publishing software
-  //                 like Aldus PageMaker including versions of Lorem Ipsum.
-  //               </h1>
-  //             </div>
-  //           </div>
+        {/* Chat Interface */}
+        <div className="w-full h-[82%]  flex justify-center pt-4">
+          <div className="w-[70%] h-full ">
+            {/* Query Text */}
+            <div className="flex items-center justify-end ">
+              <div className="max-w-[70%] bg-gray-100 p-2 rounded-md">
+                <h1 className="text-black">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industrys
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </h1>
+              </div>
+            </div>
 
-  //           {/* Response  Text */}
-  //           <div className="flex items-center ">
-  //             <div className="max-w-[70%] p-2 rounded-md text-white flex flex-row space-x-2">
-  //               <h1> &#x2022; </h1>
-  //               <h1 className="text-white">AI response</h1>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
+            {/* Response  Text */}
+            <div className="flex items-center ">
+              <div className="max-w-[70%] p-2 rounded-md text-white flex flex-row space-x-2">
+                <h1> &#x2022; </h1>
+                <h1 className="text-white">AI response</h1>
+              </div>
+            </div>
+          </div>
+        </div>
 
-  //       {/* Input  */}
-  //       <div className="w-full h-[8%] flex justify-center items-center">
-  //         <div className="h-14 w-[60%] bg-white rounded-full pl-6 pr-3 flex items-center border-2 border-black justify-between">
-  //           <input
-  //             className=" w-[94%] h-11 text-black font-sans font-medium focus:outline-none focus:ring-0"
-  //             placeholder="Ask document a question."
-  //             type="text"
-  //           />
-  //           <div className="bg-black h-10 w-10 rounded-full flex justify-center items-center">
-  //             <svg
-  //               xmlns="http://www.w3.org/2000/svg"
-  //               fill="none"
-  //               viewBox="0 0 24 24"
-  //               strokeWidth={1.5}
-  //               stroke="currentColor"
-  //               className="size-6 text-white"
-  //             >
-  //               <path
-  //                 strokeLinecap="round"
-  //                 strokeLinejoin="round"
-  //                 d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
-  //               />
-  //             </svg>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
+        {/* Input  */}
+        <div className="w-full h-[8%] flex justify-center items-center">
+          <div className="h-14 w-[60%] bg-white rounded-full pl-6 pr-3 flex items-center border-2 border-black justify-between">
+            <input
+              className=" w-[94%] h-11 text-black font-sans font-medium focus:outline-none focus:ring-0"
+              placeholder="Ask document a question."
+              type="text"
+            />
+            <div className="bg-black h-10 w-10 rounded-full flex justify-center items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-  //   // <h1 className="text-black bg-red-300">{data}</h1>
-  // );
+    // <h1 className="text-black bg-red-300">{data}</h1>
+  );
 }
 
 // export async function getServerSideProps() {
--- src/server.py
+++ src/server.py
@@ -1,33 +1,22 @@
-# from flask import Flask, jsonify
-
-# app = Flask(__name__)
-
-# @app.route('/', methods=["POST"])
-# def hello():
-#     return jsonify({"results": "Hellow World"})
-
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-#     # show the post with the given id, the id is an integer
-#     return f'Post {post_id}'
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify 
+from flask_cors import CORS 
 import os
+import chromadb
+import fitz
+import spacy
+import re
+from chromadb.utils import embedding_functions
 
 app = Flask(__name__)
 CORS(app)
 
+nlp = spacy.load("en_core_web_sm")  
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', "pdf"}
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
 # making a dir in the server 
 # This will need to be a cloud server and having a database that maps the users 
-
 os.makedirs(UPLOAD_FOLDER, exist_ok=True) 
 
 def allowed_file(filename):
@@ -52,6 +41,138 @@ def upload_file():
     filepath = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(filepath)
     return jsonify({'message': 'File uploaded successfully', 'filepath': filepath}), 200
+
+def format_text(text:str) -> str: return text.replace("\n", " ").strip()
+
+def extract_text_from_pdf(file_path):
+    # Open the file and read it as bytes
+    with open(file_path, "rb") as pdf_file:
+        pdf_bytes = pdf_file.read()  # Read the entire PDF as bytes
+        with fitz.open(stream=pdf_bytes, filetype="pdf") as doc:
+            pages_text = []
+            for page_number, page in enumerate(doc):
+                text = page.get_text()
+                text = format_text(text)  # Apply your custom text formatting
+                pages_text.append({
+                    "page_number": page_number + 1,
+                    "text": str(text),
+                })
+    return pages_text
+
+def process_text(pages_text, slice_size=12):
+    long_chunks = []
+    long_chunks_metadata = []
+
+    # Function to split sentences into chunks
+    def sentence_split(sentences_list: list[str], slice_size: int) -> list[list[str]]:
+        return [sentences_list[i:i + slice_size] for i in range(0, len(sentences_list), slice_size)]
+
+    # Process each item in pages_text
+    for item in pages_text:
+        try:
+            if isinstance(item["text"], str):
+                # Split text into sentences
+                doc = nlp(item["text"])
+                item["sentences"] = [str(sentence) for sentence in list(doc.sents)]
+                item["number_sentences"] = len(item["sentences"])
+
+                # Split sentences into chunks
+                item["chunk_sentences"] = sentence_split(sentences_list=item["sentences"], slice_size=slice_size)
+                item["number_chunk_sentences"] = len(item["chunk_sentences"])
+
+                # Combine chunks into a string and clean up
+                for chunk in item["chunk_sentences"]:
+                    chunks_dict = {}
+                    chunks_dict["page_number"] = item["page_number"]
+
+                    joined_sent_chunks = "".join(chunk).replace("\xa0", "").strip()
+                    joined_sent_chunks = re.sub(r'\.([A-Z])', r'. \1', joined_sent_chunks)  # Add space after a complete sentence
+
+                    long_chunks.append(joined_sent_chunks)
+                    long_chunks_metadata.append(chunks_dict)
+            else:
+                raise TypeError("Item text is not a string")
+        except Exception as e:
+            print(f"Error processing item: {e}")
+
+    # Return Long chunks, metadata (pages number) and idx for long chunk (used in chromadb)
+    return long_chunks, long_chunks_metadata
+
+
+
+def list_pdf_files():
+    """
+    List PDF files in the current directory
+    
+    :return: List of PDF filenames
+    """
+    pdf_files = [f for f in os.listdir('./uploads') if f.lower().endswith('.pdf')]
+    return pdf_files
+
+
+file_path = list_pdf_files()[0]
+
+print(file_path)
+
+def chroma_vector():
+    """
+    Create a ChromaDB vector collection from a PDF file
+    
+    :param file_path: Path to the PDF file
+    :return: ChromaDB collection or None if file path is invalid
+    """
+    # Check if file path is empty or None
+    # if not file_path or not isinstance(file_path, str):
+    #     print("Error: Invalid file path. Please provide a valid file path.")
+    #     return None
+    
+    # # Check if file exists
+    # if not os.path.exists(file_path):
+    #     print(f"Error: File not found at {file_path}")
+    #     return None
+    
+
+    # Extract text from PDF (you'll need to implement or import this function)
+    page_text = extract_text_from_pdf(f'./uploads/{file_path}')
+    
+    # Process text into chunks (you'll need to implement or import this function)
+    long_chunks, long_chunks_metadata = process_text(page_text)
+    
+    # Initialize ChromaDB client
+    chroma_client = chromadb.Client()
+    
+    # Get or create a collection
+    collection = chroma_client.get_or_create_collection(
+        name="current_pdf"
+        # Uncomment and configure embedding function if needed
+        # embedding_function=sentence_transformer
+    )
+    
+    # Generate sequential IDs
+    idx = [str(i) for i in range(0, len(long_chunks_metadata))]
+    
+    # Add data to the collection
+    collection.add(
+        documents=long_chunks,
+        metadatas=long_chunks_metadata,
+        ids=idx
+    )
+    
+    print(f"Successfully created vector collection with {len(long_chunks)} chunks")
+    return collection
+    
+    # except Exception as e:
+    #     print(f"An error occurred while processing the file: {e}")
+    #     return None
+
+collection = chroma_vector()
+print(collection)
+results = collection.query(
+    query_texts=["What is brian name"], 
+    n_results=2
+)
+
+print(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
