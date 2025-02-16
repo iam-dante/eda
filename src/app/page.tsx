31a30630ec3587a923c79b1f3f621fd89c14f8fb
Brian Temu <bryangasper2124@gmail.com>
2025-02-16 14:32:11 -0500

    ADD: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="div-class flex min-h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col justify-center items-center space-y-4 text-center max-w-3xl mx-auto">
-        <div className="p-1 w-32 text-center rounded-full font-semibold text-sm sm:text-base font-sans border-2 border-orange-500 ">
+        <div className="p-1 w-32 text-center rounded-full font-semibold text-sm sm:text-base font-sans border-2 border-orange-500 text-orange-500">
           Introducing
         </div>
         <div className="text-3xl sm:text-4xl md:text-5xl font-bold font-sans flex flex-row space-x-2">
