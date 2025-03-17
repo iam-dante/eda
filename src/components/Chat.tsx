0505002b83f55cea8d59b87554b80eb698e2e29f
Brian Temu <bryangasper2124@gmail.com>
2025-03-17 12:47:19 -0400

    ADD: streaming

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -8,26 +8,29 @@ import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import axios from "axios";
 import { Tab } from "@headlessui/react"; // Update this line
-
-interface Message {
-  id: string;
-  content: string;
-  sender: "user" | "ai";
-  attachment?: string;
-}
+import { useChat } from "@ai-sdk/react";
 
 export default function Chat({ chatId }: { chatId: string }) {
   const [isInitialUploadDone, setIsInitialUploadDone] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [input, setInput] = useState("");
+  // const [messages, setMessages] = useState<Message[]>([]);
+  // const [input, setInput] = useState("");
   const [attachment, setAttachment] = useState<File | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [uploadedFileName, setUploadedFileName] = useState<string>("");
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [document, setDocument] = useState<string>("");
   const { toast } = useToast();
 
+
   const bottomRef = useRef<HTMLDivElement>(null);
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    // api: "/api/chat",
+    body: {
+      // Your additional argument here
+      document: document,
+    },
+  });
 
   useEffect(() => {
     bottomRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -36,74 +39,74 @@ export default function Chat({ chatId }: { chatId: string }) {
   const production_url = "https://web-rag.onrender.com";
   const local_url = "http://127.0.0.1:5000";
 
-  const sendMessage = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (input.trim() || attachment) {
-      setIsLoading(true);
-      const newMessage: Message = {
-        id: Date.now().toString(),
-        content: input.trim(),
-        sender: "user",
-        attachment: attachment ? URL.createObjectURL(attachment) : undefined,
-      };
-      setMessages([...messages, newMessage]);
-      setInput("");
-      setAttachment(null);
+  // const sendMessage = async (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   if (input.trim() || attachment) {
+  //     setIsLoading(true);
+  //     const newMessage: Message = {
+  //       id: Date.now().toString(),
+  //       content: input.trim(),
+  //       sender: "user",
+  //       attachment: attachment ? URL.createObjectURL(attachment) : undefined,
+  //     };
+  //     setMessages([...messages, newMessage]);
+  //     setInput("");
+  //     setAttachment(null);
 
-      try {
-        const response = await fetch("http://127.0.0.1:5000/search", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify({ text: input }),
-        });
+  //     try {
+  //       const response = await fetch("http://127.0.0.1:5000/search", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           Accept: "application/json",
+  //         },
+  //         body: JSON.stringify({ text: input }),
+  //       });
 
-        let data;
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.includes("application/json")) {
-          data = await response.json();
-        } else {
-          const textResponse = await response.text();
-          throw new Error(`Invalid response format: ${textResponse}`);
-        }
+  //       let data;
+  //       const contentType = response.headers.get("content-type");
+  //       if (contentType && contentType.includes("application/json")) {
+  //         data = await response.json();
+  //       } else {
+  //         const textResponse = await response.text();
+  //         throw new Error(`Invalid response format: ${textResponse}`);
+  //       }
 
-        if (response.ok) {
-          const aiResponse: Message = {
-            id: (Date.now() + 1).toString(),
-            content:
-              data["results"] || "Sorry, I couldn't process that request.",
-            sender: "ai",
-          };
-          setMessages((prevMessages) => [...prevMessages, aiResponse]);
-        } else {
-          toast({
-            className: cn(
-              "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
-            ),
-            title: "Error",
-            description:
-              data.error || "An error occurred while processing your request.",
-            variant: "destructive",
-          });
-        }
-      } catch (error) {
-        console.error("Error details:", error);
-        toast({
-          className: cn("top-4 right-0 flex fixed md:max-w-[420px] md:right-4"),
-          title: "Error",
-          description:
-            error instanceof Error
-              ? `Failed to process request: ${error.message}`
-              : "An unexpected error occurred",
-          variant: "destructive",
-        });
-      } finally {
-        setIsLoading(false);
-      }
-    }
-  };
+  //       if (response.ok) {
+  //         const aiResponse: Message = {
+  //           id: (Date.now() + 1).toString(),
+  //           content:
+  //             data["results"] || "Sorry, I couldn't process that request.",
+  //           sender: "ai",
+  //         };
+  //         setMessages((prevMessages) => [...prevMessages, aiResponse]);
+  //       } else {
+  //         toast({
+  //           className: cn(
+  //             "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4"
+  //           ),
+  //           title: "Error",
+  //           description:
+  //             data.error || "An error occurred while processing your request.",
+  //           variant: "destructive",
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.error("Error details:", error);
+  //       toast({
+  //         className: cn("top-4 right-0 flex fixed md:max-w-[420px] md:right-4"),
+  //         title: "Error",
+  //         description:
+  //           error instanceof Error
+  //             ? `Failed to process request: ${error.message}`
+  //             : "An unexpected error occurred",
+  //         variant: "destructive",
+  //       });
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  // };
 
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -137,6 +140,7 @@ export default function Chat({ chatId }: { chatId: string }) {
         setAttachment(file);
         setIsInitialUploadDone(true);
         setUploadedFileName(file.name); // Store the filename
+        setDocument(response.data.document);
         toast({
           className: cn(
             "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4 bg-green-700 text-white"
@@ -190,9 +194,9 @@ export default function Chat({ chatId }: { chatId: string }) {
             <h1 className="font-jet text-md">Create Question</h1>
           </Tab>
         </Tab.List>
-        <Tab.Panels className="flex-1">
+        <Tab.Panels className="flex-1 overflow-hidden">
           <Tab.Panel className="h-full">
-            <div className="h-full bg-yellow-300 mt-2">
+            <div className="h-full bg-yellow-300 mt-2 overflow-hidden">
               <div className="h-[85%] overflow-y-auto px-64">
                 <div className="space-y-2 flex flex-col min-h-full w-full max-w-[calc(100vw-512px)]">
                   {messages.map((message) => (
@@ -202,7 +206,7 @@ export default function Chat({ chatId }: { chatId: string }) {
                     >
                       <div
                         className={`px-4 py-1 rounded-l-md rounded-tr-md font-sans font-medium break-words whitespace-pre-wrap ${
-                          message.sender === "user"
+                          message.role === "user"
                             ? "bg-gray-200 justify-self-end max-w-[70%]"
                             : "max-w-[90%] text-[17px] space-y-2"
                         }`}
@@ -212,7 +216,7 @@ export default function Chat({ chatId }: { chatId: string }) {
                           hyphens: "auto",
                         }}
                       >
-                        {message.sender === "ai" && (
+                        {message.role === "assistant" && (
                           <div>
                             {" "}
                             <div className="flex flex-col">
@@ -223,10 +227,10 @@ export default function Chat({ chatId }: { chatId: string }) {
                         )}
 
                         <MarkdownRenderer content={message.content} />
-                        {message.attachment && (
+                        {attachment && (
                           <div className="mt-2">
                             <a
-                              href={message.attachment}
+                              href={attachment}
                               target="_blank"
                               rel="noopener noreferrer"
                               className="text-blue-500 underline"
@@ -248,17 +252,17 @@ export default function Chat({ chatId }: { chatId: string }) {
                 </div>
               </div>
               <div className="h-[15%] py-4 px-72">
-                <form onSubmit={sendMessage} className="flex items-end w-full">
+                <form onSubmit={handleSubmit} className="flex items-end w-full">
                   <div className="w-full flex justify-center items-center">
                     <div className="h-24 w-full bg-white rounded-md pl-6 pr-3 flex items-center border-2 border-black justify-between">
                       <div className="flex items-center w-full">
-                        <Input
+                        <input
                           type="text"
                           disabled={
                             isLoading || (!isInitialUploadDone && !attachment)
                           } // Modified this line
                           value={input}
-                          onChange={(e) => setInput(e.target.value)}
+                          onChange={handleInputChange}
                           placeholder={
                             isUploading
                               ? "Uploading file..."
@@ -269,7 +273,7 @@ export default function Chat({ chatId }: { chatId: string }) {
                               : "Type your message..."
                           }
                           className={cn(
-                            "w-full h-24 text-black border-white font-sans font-medium focus:outline-none focus:ring-0 border-0",
+                            "w-full h-22 text-black bg-white border-white font-sans font-medium focus:outline-none focus:ring-0 border-0",
                             !isInitialUploadDone &&
                               !attachment &&
                               "cursor-not-allowed"
